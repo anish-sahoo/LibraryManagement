459f677b6d423c20fecfc620dcff2fb858844235
Anish Sahoo <anishsahoo2005@gmail.com>
2023-07-28 20:26:05 +0530

    Adding jsconfig file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from 'reportWebVitals';
 
-import App from './App';
-import './stylesheets/index.css';
+import App from 'App';
+import 'styles/index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
