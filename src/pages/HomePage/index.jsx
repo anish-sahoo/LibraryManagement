d782d38353907f1dee839c57aea97eb359d63beb
Anish Sahoo <anishsahoo2005@gmail.com>
2023-08-04 12:35:46 -0500

    added login button to homepage

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,10 +1,21 @@
+import { Button } from 'components/ui/button';
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const HomePage = () => {
+  const navigate = useNavigate();
+
+  const goToLogin = () => {
+    navigate('/login', { replace: true });
+  }
+
   return (
-    <h1 className='text-3xl font-bold'>
-      This is home page..!!
-    </h1>
+    <div>
+      <h1 className='text-3xl font-bold'>
+        This is home page..!!
+      </h1>
+      <Button onClick={goToLogin}>Login</Button>
+    </div>
   )
 }
 
