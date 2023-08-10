d38f5a37f7b61fa5c78a6a2975c08a2cd486f5e1
Palash Bera <palashbera1234@gmail.com>
2023-08-10 19:31:57 +0530

    Updating UI for books and users

--- src/pages/BooksPage/index.jsx
+++ src/pages/BooksPage/index.jsx
@@ -1,21 +1,59 @@
 import React, { useState, useEffect } from 'react';
-// import { useToast } from 'components/ui/use-toast';
-// import { sleep } from 'lib/utils';
-// import Loader from  'components/Loader';
+import { useToast } from 'components/ui/use-toast';
+import { sleep } from 'lib/utils';
+import Loader from  'components/Loader';
 import { Button } from 'components/ui/button'
 import { PlusIcon } from '@radix-ui/react-icons'
 import { ScrollArea } from "components/ui/scroll-area"
 import { Input } from 'components/ui/input'
-// import BookCard from './BookCard';
-import { BookCardList } from './BookCardList';
+import BookCard from "./BookCard";
 
 const BooksPage = () => {
+  const { toast } = useToast();
+  const [books, setBooks] = useState([]);
+  const [loader, setLoader] = useState(true);
+  const [totalRecords, setTotalRecords] = useState(0);
+
+  useEffect(() => {
+    fetchBooksList();
+  }, []);
+
+  const fetchBooksList = async () => {
+    try {
+      await sleep(2000);
+
+      fetch('./database/books.json')
+        .then(response => response.json())
+        .then(data => {
+          setBooks(data);
+          setTotalRecords(data.length);
+        })
+        .catch(error => console.log(error));
+    } catch (e) {
+      toast({
+        variant: "destructive",
+        title: "Something went wrong!",
+        description: "Please try again after some time.",
+      });
+    } finally {
+      setLoader(false);
+    }
+  }
+
+  if (loader) {
+    return (
+      <div className="h-full">
+        <Loader size='md' />
+      </div>
+    );
+  }
+
   return (
     <div>
-      <div className="p-6 bg-white flex justify-between items-center border-b">
+      <div className="px-6 py-4 bg-white flex justify-between items-center border-b">
         <div>
           <h2 className="text-xl font-bold tracking-tight">Books</h2>
-          <p className="text-muted-foreground">Total records: 500</p>
+          <p className="text-muted-foreground">Total records: {totalRecords}</p>
         </div>
 
         <Button>
@@ -37,7 +75,9 @@ const BooksPage = () => {
           </div>
 
           <ScrollArea className='px-4' style={{ height: "calc(100vh - 185px)" }}>
-            <BookCardList />
+            <div className="grid grid-cols-2 gap-4">
+              {books.map(book => <BookCard book={book} key={book.id} />)}
+            </div>
           </ScrollArea>
         </div>
       </div>
